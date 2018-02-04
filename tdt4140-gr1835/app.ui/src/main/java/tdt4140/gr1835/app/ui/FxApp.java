b122f94c0047468ca19939cbc1ee5b427e05a45b
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2018-02-04 22:12:26 +0100

    Lagt til et eksempel for å koble opp CSS. #5

--- tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/FxApp.java
+++ tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/FxApp.java
@@ -12,6 +12,8 @@ public class FxApp extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FxApp.fxml"));
         Scene scene = new Scene(root);
+ //Neste linje er jeg litt usikker på hvorfor funker. Virker som et kall på getResource henter filer fra resourcemappen
+        scene.getStylesheets().add(FxApp.class.getResource("stylesheet.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
--- tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/FxAppController.java
+++ tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/FxAppController.java
@@ -1,4 +1,8 @@
 package tdt4140.gr1835.app.ui;
 
 public class FxAppController {
+	
+	public void handleButton() {
+		System.out.println("Knapp Trykket");
+	}
 }
