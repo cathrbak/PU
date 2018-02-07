424f13fe67c3d7ae60490e29a5a470ed2104b3f4
Carl lystad <carllystad@dhcp-10-22-13-32.wlan.ntnu.no>
2018-02-07 12:36:54 +0100

    ikno kødd

--- tdt4140-gr1835/app.core/src/test/java/tdt4140/gr1835/app/core/Test.java
+++ tdt4140-gr1835/app.core/src/test/java/tdt4140/gr1835/app/core/Test.java
@@ -14,10 +14,8 @@ public class Test extends TestCase{
 	// NORA :O :O :O
 	// Cathrine :/
 	// Hans Kristian <3 <3 <3 
-<<<<<<< HEAD
 	// Janine B-) B-) B-)@
 	// Carl, bra vi ikke lekte soggy biscuit
-=======
 	// Janine B) B) B)
->>>>>>> 7bfb5360e473da6f7569d0b7ebbbdcf05f3777a8
+
 }
