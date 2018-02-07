328966f6019e3154187575cf1289d405f08a2752
hanskristian <hanskristian@dhcp-10-22-5-202.wlan.ntnu.no>
2018-02-07 13:02:15 +0100

    Carl er digg

---
+++ tdt4140-gr1835/app.core/src/test/java/tdt4140/gr1835/app/core/Test.java
@@ -0,0 +1,20 @@
+package tdt4140.gr1835.app.core;
+
+import junit.framework.TestCase;
+
+public class Test extends TestCase{
+
+	public void testAll(){
+		assertEquals(true, true);
+	}
+	
+	//Kommenter inn navnet ditt her
+	// Sverre :D :D :D
+	// Jonas :( :( :( 
+	// NORA :D:D:D
+	// Cathrine :/
+	// Hans Kristian <3 <3 <3 
+	// Janine B) B) B-)
+	// Carl sliter heheh
+	
+}
