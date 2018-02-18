52751e8b3c5a624bebc650b6d791b464966ea329
noragk <nora.kallager@gmail.com>
2018-02-18 18:57:06 +0100

    Begynt å opprette UI for oppretting av bruker

--- tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/FxAppController.java
+++ tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/FxAppController.java
@@ -1,8 +1,15 @@
 package tdt4140.gr1835.app.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class FxAppController {
 	
 	public void handleButton() {
-		System.out.println("Knapp Trykket");
+		//System.out.println("Knapp Trykket");
+		System.out.println(brukernavn.getText());
 	}
+	@FXML
+	TextField brukernavn;
+	
 }
