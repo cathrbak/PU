7489e5042eb83736a4819f1989f5882d344e6519
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2018-03-01 14:16:16 +0100

    prøver igjen

--- tdt4140-gr1835/app.ui/src/test/java/tdt4140/gr1835/app/ui/FxAppTest.java
+++ tdt4140-gr1835/app.ui/src/test/java/tdt4140/gr1835/app/ui/FxAppTest.java
@@ -1,20 +1,14 @@
 package tdt4140.gr1835.app.ui;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
-
-import static org.junit.Assert.*;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
+
 
 public class FxAppTest extends ApplicationTest {
 	
@@ -44,15 +38,5 @@ public class FxAppTest extends ApplicationTest {
     @Test
     public void testFxApp() {
     }
-    
-    @Before
-    public void setUp () throws Exception {
-    }
 
-    @After
-    public void tearDown () throws Exception {
-      FxToolkit.hideStage();
-      release(new KeyCode[]{});
-      release(new MouseButton[]{});
-    }
 }
