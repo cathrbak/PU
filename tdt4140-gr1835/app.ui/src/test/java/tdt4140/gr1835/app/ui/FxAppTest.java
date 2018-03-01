4e2d914a62809267197cefcb3443846f100a6b53
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2018-03-01 14:10:57 +0100

    Prøver å fikse det nora fucket opp

--- tdt4140-gr1835/app.ui/src/test/java/tdt4140/gr1835/app/ui/FxAppTest.java
+++ tdt4140-gr1835/app.ui/src/test/java/tdt4140/gr1835/app/ui/FxAppTest.java
@@ -1,12 +1,19 @@
 package tdt4140.gr1835.app.ui;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.Assert.*;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 
 public class FxAppTest extends ApplicationTest {
@@ -36,5 +43,16 @@ public class FxAppTest extends ApplicationTest {
 
     @Test
     public void testFxApp() {
+    }
+    
+    @Before
+    public void setUp () throws Exception {
+    }
+
+    @After
+    public void tearDown () throws Exception {
+      FxToolkit.hideStage();
+      release(new KeyCode[]{});
+      release(new MouseButton[]{});
     }
 }
