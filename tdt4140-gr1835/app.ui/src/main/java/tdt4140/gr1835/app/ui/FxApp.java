0922f82c466dff8e8b0f358a5d67f26aaa2032c6
janinestang <helenejs@stud.ntnu.no>
2018-03-02 18:09:55 +0100

    Prøvd å vise bilder på login-siden (ikke vellykket...) - nora&janine

--- tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/FxApp.java
+++ tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/FxApp.java
@@ -1,27 +1,28 @@
 package tdt4140.gr1835.app.ui;
 
-import javafx.application.Application;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FxApp extends Application {
+	
+	
 
     @Override
     public void start(Stage stage) throws Exception {
+    	
         Parent root_login = FXMLLoader.load(getClass().getResource("Login.fxml"));
         Parent root_nybruker = FXMLLoader.load(getClass().getResource("Ny_Bruker.fxml"));
-
+        
         Scene scene_login = new Scene(root_login);
         Scene scene_nybruker = new Scene(root_nybruker);
         scene_login.getStylesheets().add(FxApp.class.getResource("stylesheet.css").toExternalForm());
         scene_nybruker.getStylesheets().add(FxApp.class.getResource("stylesheet.css").toExternalForm());
         stage.setScene(scene_login);
         stage.show();
-
-        
         
     }
 
--- tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/LoginController.java
+++ tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/LoginController.java
@@ -1,6 +1,5 @@
 package tdt4140.gr1835.app.ui;
 
-
 import java.io.IOException;
 
 import javafx.fxml.FXML;
@@ -10,6 +9,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class LoginController {
@@ -24,16 +25,20 @@ public class LoginController {
 	@FXML
 	Button button_nybruker;
 	
-	
 	@FXML
 	Label responsLabel;
 	
+	@FXML
+	ImageView butterflyImage;
+
+
 	@FXML
 	public void handleNyBrukerButton() throws IOException {
 		Stage stage; 
 	    Parent root;
         //get reference to the button's stage         
         stage=(Stage) button_nybruker.getScene().getWindow();
+        
         //load up OTHER FXML document
         root = FXMLLoader.load(getClass().getResource("Ny_Bruker.fxml"));
       	//create a new scene with root and set the stage
@@ -60,10 +65,8 @@ public class LoginController {
 	        stage.setScene(scene);
 	        stage.show();
 		}
-		
-		
-	}
 
+	}
 	private boolean loginOk() {
 		// TODO Auto-generated method stub
 		return true;
