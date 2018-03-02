62192152e3550ec3cb4007b73be29f3b5596f64c
Cathrine Akre-Aas <cathrine-97@hotmail.com>
2018-03-02 18:49:21 +0100

    Har prøvd å få til at "profil"-knappen funker, men det funker ikke

--- tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/MainPageController.java
+++ tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/MainPageController.java
@@ -1,6 +1,54 @@
 package tdt4140.gr1835.app.ui;
 
-public class MainPageController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+public class MainPageController implements Initializable {
+	
+	@FXML
+	Menu meny;
+	@FXML
+	MenuItem Profile;
+	
+	@FXML
+	MenuItem Settings;
+	
+	@FXML
+	MenuItem logOut;
+	
+	
+	@FXML
+	public void handleProfileMenuItem(ActionEvent event) throws IOException {
+		Stage stage; 
+		Parent root;
+	    
+        //get reference to the button's stage         
+        stage=(Stage) Profile.getOnAction();
+        //load up OTHER FXML document
+        root = FXMLLoader.load(getClass().getResource("Profil.fxml"));
+      	//create a new scene with root and set the stage
+        Scene scene = new Scene(root);
+        //Legger på css stylesheetet
+        scene.getStylesheets().add(FxApp.class.getResource("stylesheet.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+	}
+	
+	 @Override
+	    public void initialize(URL url, ResourceBundle rb) {
+	        // TODO
+	    }
 	
 	
 
