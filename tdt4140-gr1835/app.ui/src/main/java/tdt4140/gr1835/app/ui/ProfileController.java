b5c2653627f6af8e9cfd8db8de6197e35b73e08f
noragk <nora.kallager@gmail.com>
2018-03-07 12:27:57 +0100

    Har lagt til funksjonalitet i knappene som ikke hadde det, samt koblet alle elementer i fxml-filene til css-filen.

--- tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/ProfileController.java
+++ tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/ProfileController.java
@@ -1,5 +1,36 @@
 package tdt4140.gr1835.app.ui;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class ProfileController {
 
+	//Tilbakeknapp
+		@FXML
+		Button BackToMainButton;
+		@FXML 
+		public void handleBackToMainButton() throws IOException {
+			Stage stage; 
+		    Parent root;
+	        //get reference to the button's stage         
+	        stage=(Stage) BackToMainButton.getScene().getWindow();
+	        //load up OTHER FXML document
+	        root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+	      	//create a new scene with root and set the stage
+	        Scene scene = new Scene(root);
+	        //Legger på css stylesheetet
+	        scene.getStylesheets().add(FxApp.class.getResource("stylesheet.css").toExternalForm());
+	        stage.setScene(scene);
+	        stage.show();
+		}
+	
+	
+	
+	
 }
