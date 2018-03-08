e20cb43bbfa8cf8ad73fe76da727550020a759b1
janinestang <helenejs@stud.ntnu.no>
2018-03-08 15:58:32 +0100

    Fortsatt på test for oppretting av bruker : "Prøve å sette inn noen ugyldige verdier i opprett bruker vinduet, trykke "opprett ny bruker", endre til gyldig oppsett, så sjekke at objektet er opprettet"
    tdt4140-2018/35#33

--- tdt4140-gr1835/app.ui/src/test/java/tdt4140/gr1835/app/ui/FxAppTest.java
+++ tdt4140-gr1835/app.ui/src/test/java/tdt4140/gr1835/app/ui/FxAppTest.java
@@ -2,14 +2,21 @@ package tdt4140.gr1835.app.ui;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.testfx.api.FxAssert;
 import org.testfx.framework.junit.ApplicationTest;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+//import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.base.NodeMatchers.*;
+import static org.hamcrest.CoreMatchers.*;
+
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
@@ -17,6 +24,8 @@ import javafx.stage.Stage;
 public class FxAppTest extends ApplicationTest {
 	
 	Button btn;
+	
+	Label infotext;
 	
 	@BeforeClass
 	public static void headless() {
@@ -40,6 +49,46 @@ public class FxAppTest extends ApplicationTest {
         stage.setScene(scene);
         stage.show();
     }
+	
+	@Test
+	public void testFeilBrukerInnlogging() {
+		
+	// Klikker meg over til Opprett ny bruker vinduet
+	// Man bruker fx:id identifikatoren fra FXML filen inne i clickOn metoden
+		clickOn("#button_nybruker");
+	
+	// Trykker videre på first name tekstboksen, fyller inn, og bruker tab for å komme til neste felt
+	// Feil inntasting
+	    	clickOn("#firstName");
+		write("N0ra");
+		push(KeyCode.TAB);
+		write("Spetalen");
+		push(KeyCode.TAB);
+		write("sverress");
+		push(KeyCode.TAB);
+		write("sss");
+		push(KeyCode.TAB);
+		write("sss");
+		push(KeyCode.TAB);
+		write("sverress@stud.ntnu.no");
+		push(KeyCode.TAB);
+		write("sverress@stud.ntnu.no");
+		push(KeyCode.TAB);
+		write("46839737");
+		push(KeyCode.TAB);
+		write("IE");
+		
+		// prøver å lage ny ugyldig bruker
+		clickOn("#button_registrer");
+		
+		try {
+			FxAssert.verifyThat("#infotext", hasText("Kan ikke opprette ugyldig bruker"));
+		}
+		catch (IllegalArgumentException e){
+			assertFalse("Fikk IllegalArgumentException etter å sette inn gyldig argument",e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
 
     @Test
     public void testNyBrukerInnlogging() {
@@ -49,6 +98,7 @@ public class FxAppTest extends ApplicationTest {
     		clickOn("#button_nybruker");
     		
     	//Trykker videre på first name tekstboksen, fyller inn, og bruker tab for å komme til neste felt.
+    	// Riktig inntasting 
     		clickOn("#firstName");
     		write("Sverre");
     		push(KeyCode.TAB);
@@ -68,11 +118,15 @@ public class FxAppTest extends ApplicationTest {
     		push(KeyCode.TAB);
     		write("IE");
     		
-    	//Prøver å lage ny bruker
+    	//Prøver å lage ny gyldig bruker
     		clickOn("#button_registrer");
     	
-
-    	    assertTrue(true);
+    		try {
+    			FxAssert.verifyThat("#infotext", hasText("Det eksisterer en bruker med dette brukernavnet"));
+    		}
+    		catch (IllegalArgumentException e){
+    			assertFalse("Fikk IllegalArgumentException etter å sette inn gyldig argument",e.getClass().equals(IllegalArgumentException.class));
+    		}
     }
  
     
