8684fc4962e4bea6cffdc13de135d9a20b6d2eec
Cathrine Akre-Aas <cathrine-97@hotmail.com>
2018-03-14 18:09:30 +0100

    Laget tom konstruktør og testet dette (?) tdt4140-2018/35#74

--- tdt4140-gr1835/app.core/src/test/java/tdt4140/gr1835/app/core/TableTest.java
+++ tdt4140-gr1835/app.core/src/test/java/tdt4140/gr1835/app/core/TableTest.java
@@ -1,10 +1,5 @@
 package tdt4140.gr1835.app.core;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +15,11 @@ public class TableTest extends TestCase{
 		System.out.println("Setup");
 		testTable=new Table(1,1,2,3,4,5,1,2,3,4,5,30);
 	}
+	@Before
+	public void setUpEmpty() throws Exception {
+		System.out.println("Setup");
+		testTable=new Table(1);
+	}
 
 	@Test
 	public void testNewTable() {
@@ -34,12 +34,6 @@ public class TableTest extends TestCase{
 		Table testTable2= new Table(1,1,2,3,4,5,1,2,3,4,5,30);
 		int expected = testTable.getSpm5();
 		assertEquals(expected, testTable2.getSpm5());
-		/*try {
-			testTable2.setSpm5();
-			fail("Testen skulle ha feilet fordi ett av svarene ikke er en Integer");
-		}catch(IllegalArgumentException e) {
-			assertTrue(e.getClass().equals(IllegalArgumentException.class));
-		}*/
 		try {
 			//Table testTable2= new Table(1,1,2,3,4,50,1,2,3,4,5,30);
 			testTable2.setSpm5(50);
@@ -49,6 +43,48 @@ public class TableTest extends TestCase{
 		}
 		
 	}
+
+@Test
+public void testRightValueConstructor() {
+	System.out.println("testValue1");
+	//Table testTable2= new Table(1,1,2,3,4,5,1,2,3,4,5,30);
+	int expected = testTable.getSpm1();
+	assertEquals(expected, 1);
+	try {
+		Table testTable2 = new Table(1,50,1,1,1,1,1,1,1,1,1,30);
+		Table testTable3 = new Table(1,1,50,1,1,1,1,1,1,1,1,30);
+		Table testTable4 = new Table(1,1,1,50,1,1,1,1,1,1,1,30);
+		Table testTable5 = new Table(1,1,1,1,50,1,1,1,1,1,1,30);
+		Table testTable6 = new Table(1,1,1,1,1,50,1,1,1,1,1,30);
+		Table testTable7 = new Table(1,1,1,1,1,1,50,1,1,1,1,30);
+		Table testTable8 = new Table(1,1,1,1,1,1,1,50,1,1,1,30);
+		Table testTable9 = new Table(1,1,1,1,1,1,1,1,50,1,1,30);
+		Table testTable10 = new Table(1,1,1,1,1,1,1,1,1,50,1,30);
+		Table testTable11 = new Table(1,1,1,1,1,1,1,1,1,1,50,30);
+		fail("Testen skulle ha feilet pga at et av tallene er større enn 5");
+	}catch(IllegalArgumentException e) {
+		assertTrue(e.getClass().equals(IllegalArgumentException.class));
+	}
+	
+}
+
+@Test
+public void testSum() {
+	Table testTable2= new Table(1,1,2,3,4,5,1,2,3,4,5,30);
+	int sum;
+	int expected = testTable.getTotal();
+	assertEquals(expected, testTable2.getTotal());
+	try{
+		sum = testTable2.getSpm1() + testTable2.getSpm2() + testTable2.getSpm3() + testTable2.getSpm4() + testTable2.getSpm5() + testTable2.getSpm6() + testTable2.getSpm7() + testTable2.getSpm8() + testTable2.getSpm9() + testTable2.getSpm10(); 
+		if (testTable2.getTotal() != sum){
+			fail("Testen skulle ha feilet fordi totalen er feil");
+		}
+		
+	}
+	catch(IllegalArgumentException e) {
+		assertTrue(e.getClass().equals(IllegalArgumentException.class));
+	}
+}
 	
 	@After
 	public void tearDown() {
