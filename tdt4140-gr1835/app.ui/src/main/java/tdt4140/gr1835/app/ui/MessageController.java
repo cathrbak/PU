56e6bcf2dcf65b9d8a7cd696e48dd04d0863704d
noragk <nora.kallager@gmail.com>
2018-03-16 17:38:31 +0100

    Opprettet metode som gir beskjed om for lang melding til avsender av mld

--- tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/MessageController.java
+++ tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/MessageController.java
@@ -7,12 +7,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+import tdt4140.gr1835.app.core.Message;
 import tdt4140.gr1835.app.core.Nurse;
 
 public class MessageController {
 
 	private Nurse nurse;
+	private Message message;
 
 	public MessageController(Nurse nurse) {
 		this.nurse = nurse;
@@ -20,6 +24,8 @@ public class MessageController {
 	
 	@FXML
 	Button Button_send;
+	
+	TextArea textbox;
 	
 	@FXML
 	public void handleButton_send() throws SQLException, Exception{
@@ -60,7 +66,7 @@ public class MessageController {
         //get reference to the button's stage        
         stage=(Stage) returnButton.getScene().getWindow();
         
-        MainPageController controller= new MainPageController(this.nurse); //Lager en kontroller instans
+        MainPageController controller = new MainPageController(this.nurse); //Lager en kontroller instans
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("StudentProfil.fxml"));
             
@@ -75,4 +81,17 @@ public class MessageController {
         stage.setScene(scene);
         stage.show();
 	}
+	
+	@FXML
+	Label messageRespons;
+	
+	public void handleTextboxChange() {   			//metode for å vise felt til bruker om at melding er for lang,
+		messageRespons.setVisible(false);			//samt sette meldingsteksten til å være det brukeren skriver inn.
+		if(textbox.getText().length() > 250) {
+			textbox.setText("Meldingen er for lang. Kan ikke overgå 255 tegn.");
+			textbox.setVisible(true);
+		}
+		message.setText(textbox.getText());
+	}
+	
 }
