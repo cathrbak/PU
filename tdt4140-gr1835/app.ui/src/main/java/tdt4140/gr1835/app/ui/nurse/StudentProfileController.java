80488bd8c08027b6ac7e6aca545d35a142f18f45
Cathrine Akre-Aas <cathrine-97@hotmail.com>
2018-04-02 16:16:14 +0200

    Lagt til studentens data i tabellen tdt4140-2018/35#64

--- tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/nurse/StudentProfileController.java
+++ tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/nurse/StudentProfileController.java
@@ -1,20 +1,33 @@
 package tdt4140.gr1835.app.ui.nurse;
 
+import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import tdt4140.gr1835.app.core.ConnectionSQL;
 import tdt4140.gr1835.app.core.Nurse;
 import tdt4140.gr1835.app.core.Student;
+import tdt4140.gr1835.app.core.Table;
 import tdt4140.gr1835.app.core.UserDatabaseHandler;
 
-public class StudentProfileController {
+public class StudentProfileController implements Initializable{
 	
 	private Nurse nurse;
 	private Student student;
@@ -23,8 +36,78 @@ public class StudentProfileController {
 		this.nurse = nurse;
 		this.student = student;
 		System.out.println(student.getUsername()+" er logget på med søster: "+nurse.getUsername());
+		addInfo();
+		//addName();
+	}
+
+	/*@FXML
+	Text navn;
+	//legger til fornavn og etternavn på studentprofil:
+	public void addName(){
+		if (student.isAnonymous()) {
+			navn.setText("Fornavn Etternavn");
+		}
+		else {
+			navn.setText(student.getFirstName() + " " + student.getSecondName());			
+			}
 	}
 	
+	@FXML
+	TextField epost;
+	@FXML
+	Label label;
+	
+	//henter epost
+	public void setEmail() {
+		if (student.isAnonymous()) {
+			epost.setText("anonym@stud.ntnu.no");
+		}
+		else {
+			epost.setText(student.getEmail());
+			label.setText(student.getEmail());
+		}
+	}*/
+	
+	//lager listen som skal inneholde dataen
+	final ObservableList<Table> data = FXCollections.observableArrayList();
+	
+	@FXML 
+	TableView<Table> tableID;
+	@FXML 
+	TableColumn<Table, String> Dato;
+	@FXML 
+	TableColumn<Table, Integer> PersonID;
+	@FXML 
+	TableColumn<Table, Integer> Spm1;
+	@FXML 
+	TableColumn<Table, Integer> Spm2;
+	@FXML 
+	TableColumn<Table, Integer> Spm3;
+	@FXML 
+	TableColumn<Table, Integer> Spm4;
+	@FXML 
+	TableColumn<Table, Integer> Spm5;
+	@FXML 
+	TableColumn<Table, Integer> Spm6;
+	@FXML 
+	TableColumn<Table, Integer> Spm7;
+	@FXML 
+	TableColumn<Table, Integer> Spm8;
+	@FXML 
+	TableColumn<Table, Integer> Spm9;
+	@FXML 
+	TableColumn<Table, Integer> Spm10;
+	@FXML 
+	TableColumn<Table, Integer> Total;
+	
+	//Legger til data i tabellen:
+	public void addInfo() throws SQLException, Exception{
+		List<Table> listOfAnswers = student.getAnswers();
+			for(Table answer: listOfAnswers) {
+				data.add(answer);	
+				}
+		}
+		
 	@FXML
 	Button returnButton;
 	
@@ -83,5 +166,22 @@ public class StudentProfileController {
         stage.setScene(scene);
         stage.show();
 	}
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Dato.setCellValueFactory(new PropertyValueFactory<Table, String>("Dato"));
+		Spm1.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Spm1"));
+		Spm2.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Spm2"));
+		Spm3.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Spm3"));
+		Spm4.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Spm4"));
+		Spm5.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Spm5"));
+		Spm6.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Spm6"));
+		Spm7.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Spm7"));
+		Spm8.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Spm8"));
+		Spm9.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Spm9"));
+		Spm10.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Spm10"));
+		Total.setCellValueFactory(new PropertyValueFactory<Table, Integer>("Total"));
+	    tableID.setItems(data);
+	    }	
+	
 	
 }
