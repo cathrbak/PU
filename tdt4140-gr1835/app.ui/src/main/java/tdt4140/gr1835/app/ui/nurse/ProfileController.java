8da6f4eade7b05e5fce7f8a33bb97a9386e79e42
janinestang <helenejs@stud.ntnu.no>
2018-04-07 22:10:11 +0200

    Fornavn og etternavn til innlogget helsesøster vises nå i Profile tdt4140-2018/35#101

--- tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/nurse/ProfileController.java
+++ tdt4140-gr1835/app.ui/src/main/java/tdt4140/gr1835/app/ui/nurse/ProfileController.java
@@ -1,17 +1,20 @@
 package tdt4140.gr1835.app.ui.nurse;
 
-import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import tdt4140.gr1835.app.core.Nurse;
 
-public class ProfileController {
+public class ProfileController implements Initializable{
 	private Nurse nurse;
 	public ProfileController(Nurse nurse) {
 		this.nurse = nurse;
@@ -48,7 +51,25 @@ public class ProfileController {
             stage.show();
 		}
 	
+		// Labels for fullt navn for innlogget helsesøster
+		@FXML
+		Label fulltnavn;
+
+		// Metode for å sette fullt navn
+		@FXML
+		public void setFulltnavnLabel() {
+			fulltnavn.setText(nurse.getFirstName() + " " + nurse.getSecondName());
+		}
+		
+
+		@Override
+		public void initialize(URL location, ResourceBundle resources) {
+			// kaller på Label-metodene
+			setFulltnavnLabel();
+			
+		}
 	
-	
+
+		
 	
 }
