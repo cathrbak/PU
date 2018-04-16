8243bc3f74a26be78e82bdfa531aaf2a51286160
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2018-04-16 23:37:20 +0200

    BugFix Janine på MainPage og laget litt fler integrasjonstester

--- tdt4140-gr1835/tdt4140.gr1835.app.webserver/src/test/java/org/tdt4140/gr1835/app/webserver/resources/DatabaseWebServerWebClientintegrationIT.java
+++ tdt4140-gr1835/tdt4140.gr1835.app.webserver/src/test/java/org/tdt4140/gr1835/app/webserver/resources/DatabaseWebServerWebClientintegrationIT.java
@@ -4,11 +4,13 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import tdt4140.gr1835.app.core.Message;
 import tdt4140.gr1835.app.core.Nurse;
 import tdt4140.gr1835.app.core.Student;
+import tdt4140.gr1835.app.core.Table;
 import tdt4140.gr1835.app.webclient.RESTClient;
 import tdt4140.gr1835.app.webclient.RestClientImp;
 
@@ -31,16 +33,31 @@ public class databaseWebServerWebClientintegrationIT {
 	@Test
 	public void testGetMessages() {
 		List<Message> messagesStudent = client.getMessagesStudent("sverress");
-		messagesStudent.stream().forEach(m->System.out.println(m.getText()));
+		//messagesStudent.stream().forEach(m->System.out.println(m.getText()));
 		assertEquals("sverress", messagesStudent.get(0).getReciver().getUsername());
+		assertEquals("Heisann2", messagesStudent.get(3).getText());
 	}
 	
 	@Test
 	public void testGetStudent() {
 		Student student = client.getStudent("sverress");
 		assertEquals("sverress", student.getUsername());
+		assertEquals(1, student.getStudentID());
 	}
 	
+	@Test
+	public void testGetStudents() {
+		List<Student> students = client.getStudents("testsoster");
+		students.stream().forEach(s->System.out.println(s.getUsername()));
+		assertEquals("Tar kontakt neste uke.", students.get(0).getNotat());
+		assertEquals("norak", students.get(1).getUsername());
+	}
 	
-
+	@Test
+	public void testGetAnswers() {
+		List<Table> answers = client.getAnswers("sverress");
+		answers.stream().forEach(a->System.out.println(a.getSpm3()));
+		assertEquals(34, answers.get(0).getTotal());
+		assertEquals(5, answers.get(0).getSpm3());
+	}
 }
